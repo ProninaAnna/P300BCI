eccd48c41cb8cec2454b308fb38093bc6d78c2a7
m0rningstar <a.pronina@hotmail.com>
2022-04-13 20:55:42 +0300

    Minor changes

--- eeg.py
+++ eeg.py
@@ -1,5 +1,6 @@
 import multiprocessing
 from pylsl import StreamInlet, resolve_byprop
+from CONSTANTS import *
 
 class EEG:
     def __init__(self):
@@ -7,11 +8,17 @@ class EEG:
 
     def eeg_process(self):
         print("looking for an EEG stream...")
-        streams = resolve_byprop('name', 'NVX52_Data')
+        streams = resolve_byprop('name', EEG_STREAM)
         inlet = StreamInlet(streams[0])
         for i in range(0, 10):
             sample, timestamp = inlet.pull_sample()
-            print(timestamp, sample)
+            print('eeg ', timestamp, sample)
 
     def photocell_process(self):
-        pass
+        print("looking for a photosensor stream...")
+        streams = resolve_byprop('name', PHOTOSENSOR_STREAM)
+        inlet = StreamInlet(streams[0])
+        for i in range(0, 10):
+            sample, timestamp = inlet.pull_sample()
+            print('photo ', timestamp, sample)
+
