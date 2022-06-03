bec490f0a0dd70be41055f2d160f2c12d1409581
Timofey <timofeyponomaryov@gmail.com>
2022-06-03 16:14:23 +0300

    Intermediate test

--- CONSTANTS.py
+++ CONSTANTS.py
@@ -12,7 +12,7 @@ IP_IVIEWX = '192.168.2.43' # static IP of the iViewX laptop
 IP_STIM = '192.168.2.173' # static IP of the stimulus PC
 
 MONITOR = 'Dell' # monitor name
-MONITOR_N = 2
+MONITOR_N = 1
 SIZE = (1680,1050) # screen resolution (2560, 1440), px
 WIDTH = 47.5 # monitor screen width, 59.5 cm 
 DISTANCE = 56 # distance between subject and screen, cm
@@ -21,8 +21,8 @@ SCREEN_UNITS = 'deg' # coordinates units
 CENTER = (0,0) # fixation mark coordinates
 PHOTOSENSOR_POS = (25.5, 14.4) # photosensor stimulus coordinates
 STIM_NAMES = [item for item in u'qwertyuiopasdfghjklzxcvbnm_1234567890!?.,;:"()+=-~[]\/'.upper()] # available stimuli names, unicode
-GROUP1 = [(0, 11, 21), (1, 12, 22), (2, 13, 23), (3, 14, 24), (4, 15, 25), (5, 16, 26), (6, 17, 18), (7, 9, 19), (8, 10, 20)] # groups of stims ("rows and colums")
-GROUP2 = [(0, 17, 24), (1, 9, 25), (2, 10, 26), (3, 11, 18), (4, 12, 19), (5, 13, 20), (6, 14, 21), (7, 15, 22), (8, 16, 23)]
+GROUP1 = {0:(0, 11, 21), 1:(1, 12, 22), 2:(2, 13, 23), 3:(3, 14, 24), 4:(4, 15, 25), 5:(5, 16, 26), 6:(6, 17, 18), 7:(7, 9, 19), 8:(8, 10, 20)} # groups of stims ("rows and colums")
+GROUP2 = {9:(0, 17, 24), 10:(1, 9, 25), 11:(2, 10, 26), 12:(3, 11, 18), 13:(4, 12, 19), 14:(5, 13, 20), 15:(6, 14, 21), 16:(7, 15, 22), 17:(8, 16, 23)}
 STIM_SIZE = ( 5.5, 3.5, 1.5) # stimuli sizes
 STIM_POS = [(0,-12.000), (-7.713,-9.193), (-11.818,-2.084), 
             (-10.392,6.000), (-4.104,11.276), (4.104,11.276), 
@@ -36,9 +36,10 @@ STIM_POS = [(0,-12.000), (-7.713,-9.193), (-11.818,-2.084),
 
 FLASH = 0.025 # flashing time, s
 ISI = 0.075 # inter stimulus interval, s
-TRIAL_LEN = 1 # number of flashes in one trial
+TRIAL_LEN = 2 # number of flashes in one trial
 
 BACKCOL=(-1,-1,-1) # background color
 FIXCOL=(1,1,1) # fixation mark color
 STIMCOL=(1,1,1) # stimuli color
+QUECOL=(1.0,-1,-1) # que color
 
--- visual.py
+++ visual.py
@@ -1,6 +1,7 @@
 import multiprocessing
 import logging
 import time
+import numpy as np
 #import psychopy.visual # ?
 from psychopy.monitors import Monitor
 from psychopy.visual import Window
@@ -22,6 +23,10 @@ def create_lsl_outlet():
     outlet = StreamOutlet(info) # StreamOutlet object to stream data samples/chunks
     return outlet
 
+def merge_two_dicts(x, y):
+    z = x.copy()   # start with keys and values of x
+    z.update(y)    # modifies z with keys and values of y
+    return z
 
 class Visual:
     '''
@@ -77,14 +82,19 @@ class Visual:
             else: 
                 stim_size = STIM_SIZE[2]
 
-            # flash condition
+            # Draw stimulus
+            stim = TextStim(self.display, text=STIM_NAMES[index], pos=position, units=SCREEN_UNITS, height=stim_size, opacity=0.5)
+            
+            # Make target stimuli flash
             if state == 'flash':
                 if index in flash_group:
                     stim = TextStim(self.display, text=STIM_NAMES[index], pos=position, units=SCREEN_UNITS, height=stim_size, opacity=1)
                     self.photosensor_stim.draw()
-            else:
-                stim = TextStim(self.display, text=STIM_NAMES[index], pos=position, units=SCREEN_UNITS, height=stim_size, opacity=0.5)
-                
+
+            if state == 'que':
+                if index == flash_group:
+                    stim = TextStim(self.display, text=STIM_NAMES[index], pos=position, units=SCREEN_UNITS, height=stim_size, opacity=1, color=QUECOL)
+
             stim.draw()
             
         # drawing other stimuli
@@ -93,16 +103,26 @@ class Visual:
         self.display.flip()
         
 
-    def visual_stimulation(self, flash_group):
+    def visual_stimulation(self, flash_group, group_number):
         '''Run stimulation'''
 
         self.visual_environment(flash_group = flash_group, state='flash')
-        self.LSL.push_sample([flash_group[0]], float(time.time()))
+        self.LSL.push_sample([group_number], float(time.time()))
         wait(FLASH)
         self.visual_environment(flash_group)
         wait(ISI)
     
-    
+    def show_target(self, letter):
+        '''Show target stimulus with multiple flashes
+        
+        Keyword arguments:
+        letter -- target letter to show to user
+
+        '''
+        wait(1)
+        self.visual_environment(STIM_NAMES.index(letter), state='que')
+        wait(2)
+
     def take_screenshot(self, filepath):
         '''Draw the visual environment and take a screenshot'''
 
@@ -112,14 +132,19 @@ class Visual:
         self.display.close()
 
 
-    def visual_process(self, sequence, lockable = True):
+    def visual_process(self, sequence='', lockable = True):
         '''
         Run visual process.
         Arguments:
         
         sequence -- list of lists or similar containers with indeces of target stimuli
         '''
+        np.random.seed=42
+        groups=merge_two_dicts(GROUP1, GROUP2) # make stimulus groups dict
+        order=np.random.shuffle([i for i in range(len(groups))]) # generate random order of flashse
         try:
+            
+            # Lock Visual process if necessary 
             if lockable:
                 self.lock.acquire()
                 logging.info("Visual process locked ")
@@ -133,13 +158,17 @@ class Visual:
 
             waitKeys() # pressing any key starts the stimulation
 
-            # loop over target sequence 
-            for item in sequence:
-                # loop over trials
-                for i in range(TRIAL_LEN):
+            # loop over all target words 
+            for word in ['BCI']:
+                # loop over all target letters
+                for letter in word:
+                    # Show target stimulus
+                    self.show_target(letter)
+                    # loop over random flashes
                     self.LSL.push_sample([TRIAL_START], float(time.time()))
-    
-                    self.visual_stimulation(item)
+                    for i in range(TRIAL_LEN):
+                        for j in order:
+                            self.visual_stimulation(groups[j], j)
                     self.LSL.push_sample([TRIAL_END], float(time.time()))
 
             self.display.close()
@@ -152,7 +181,9 @@ class Visual:
 
 
 if __name__ == '__main__':
-    a=Visual()
-    a.visual_process(GROUP1+GROUP2, lockable = False)
 
-   
+    queue = multiprocessing.Queue()
+    pipe_in, pipe_out = multiprocessing.Pipe()
+    lock = multiprocessing.Lock()
+    a=Visual(queue, pipe_in, pipe_out, lock)
+    a.visual_process(lockable = False)
